61a65c87ed8dff2f5836afe18075ceb1f3d9d633
valerio <valerioamato@hotmail.it>
2023-02-27 17:11:03 +0100

    controlli

--- file1.cpp
+++ file1.cpp
@@ -11,22 +11,21 @@ int main()
     int quanteVolte = 0;
     for (int i = 0; i < inserimento.length()-1; i++)
     {
-        for (int y = 1; y < inserimento.length()-1; y++)
+        for (int y = i+1; y < inserimento.length()-1; y++)
         {
             if (inserimento[i] == inserimento[y]) // inserimento[] ==  stoi(inserimento.length()-1))
             {
                 int quanteVolte;
-                quanteVolte += y;
+                quanteVolte++;
             }
-            cout << "La lettera "
-                 << " " << inserimento[i] << " è ripetuta" << quanteVolte << "volte. Di conseguenza la parola non  è univoca";
+          else if (i < inserimento.length() - 1 )
+          {
+                       cout << "La lettera "<< " " << inserimento[i] << " è ripetuta" << quanteVolte << "volte. Di conseguenza la parola non  è univoca";
 
-            else if (i == inserimento.length() - 1)
-            {
-                break;
-            }
-            else
-            {
+          }
+
+             
+           else {
                 cout << "La parola ha caratteri univoci!!!";
                 break;
             }
